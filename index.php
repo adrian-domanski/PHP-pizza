ef67ab1ad58e5aa468519e259141b3348dd51b8b
Adrian <adrian.domanski.dev@gmail.com>
2019-07-12 17:20:22 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php require './templates/header.php' ?>
+    
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,5 @@
+<head>
+    <title>Pizza PHP</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+<body class="grey lighten-4">
